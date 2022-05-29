8939a05f8e146a506236a489a28b26ac2a60816f
Insun <inseonyu7@gmail.com>
2022-05-29 22:53:23 +0900

    chore: add edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://github.com/semaphore-onchain-group/docs",
+          editUrl: "https://github.com/semaphore-onchain-group/docs/edit/main/",
           routeBasePath: "/",
         },
         theme: {
