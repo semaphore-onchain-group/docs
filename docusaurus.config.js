74171fe3031b02a4d5d353cdcf2333ba10ff8363
Insun <inseonyu7@gmail.com>
2022-05-29 22:32:06 +0900

    chore: modify onBrokenLinks configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,22 +10,12 @@ const config = {
   tagline: "NFT on-chain group open platform",
   url: "https://nft-group-docs.vercel.app/",
   baseUrl: "/",
-  onBrokenLinks: "throw",
+  onBrokenLinks: "ignore",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-  },
+  organizationName: "semaphore-nft-group",
+  projectName: "semaphore-nft-group",
 
   presets: [
     [
